a53d1036ef7308bba52ca52d71883c8a073dc5c2
k-romero <imkevinromero@gmail.com>
2020-04-27 14:52:59 -0400

    modified

--- src/cards_components/Card.js
+++ src/cards_components/Card.js
@@ -9,10 +9,14 @@ class Card extends React.Component {
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
+          <div className="location">{profile.location}</div>
+          <div className="bio">{profile.bio}</div>
+          <div className="mainUrl">{profile.html_url}</div>
+          <div className="publicRepos">{profile.public_repos} public repos</div>
         </div>
     	</div>
     );
   }
 }
 
-export default Card;
+export default Card; 
--- src/cards_components/Form.js
+++ src/cards_components/Form.js
@@ -2,13 +2,16 @@ import React from 'react';
 import axios from 'axios';
 
 class Form extends React.Component {
+
     state = { userName: '' }
+
     handleSubmit = async (event) => {
       event.preventDefault();
-      const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`)
+      const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
       this.props.onSubmit(resp.data);
       this.setState({ userName: '' })
     };
+
     render() {
       return (
       <form onSubmit={this.handleSubmit}>
