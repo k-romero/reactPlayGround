ed77cf1f48c3ac306914b286f77f8d9689cd26ae
k-romero <imkevinromero@gmail.com>
2020-04-27 16:52:09 -0400

    restructured sandbox

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.scss';
 
-import UserCards from './cards_components/UserCards'
+import UserCards from './components/github_card/UserCards'
 
 function App() {
   return (
--- src/components/github_card/Form.js
+++ src/components/github_card/Form.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 
+
 class Form extends React.Component {
 
     state = { userName: '' }
-
+    
     handleSubmit = async (event) => {
       event.preventDefault();
       const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
