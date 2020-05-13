b372ff5a359b7e8205b1da21e04716f5feb039e7
k-romero <imkevinromero@gmail.com>
2020-05-12 21:57:35 -0400

    removing large heading on projcards

--- src/components/portfolio/projects/ProjCard.js
+++ src/components/portfolio/projects/ProjCard.js
@@ -19,9 +19,9 @@ export default class ProjCard extends React.Component {
         onClick={() => this.handleClick()}
       >
         <div className="">
-          <h2 className="heading-secondary heading-secondary-light">
+          {/* <h2 className="heading-secondary heading-secondary-light">
             {this.props.heading}
-          </h2>
+          </h2> */}
         </div>
         <div className="">
           <div className="">
